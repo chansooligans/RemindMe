c406d83498d15e3ca27367cc1ab30774ae73d8ea
chansooligans <cs2737@nyu.edu>
2023-04-29 10:36:34 -0400

    update pre-commits; separate parser and handler

--- remindme/main.py
+++ remindme/main.py
@@ -1,5 +1,12 @@
+import datetime
 from dataclasses import dataclass
-from remindme import parsers
+
+import googleapiclient
+from django.db import models
+from django.http import HttpResponse
+from twilio.twiml.messaging_response import MessagingResponse
+
+from remindme import calendar, parsers
 
 
 @dataclass
@@ -19,7 +26,7 @@ class OpenAIParser:
 
 
 @dataclass
-class MessageHandler:
+class MessageParser:
     message_body: str
 
     def __post_init__(self):
@@ -30,3 +37,54 @@ class MessageHandler:
 
     def parse_message(self):
         return self.parser.parse()
+
+
+@dataclass
+class RequestHandler:
+    parsed: str
+    calendar_service: googleapiclient.discovery.Resource
+    ScheduledEvent: models.Model
+    sender_phone_number: str
+
+    def process_request(self):
+        if self.parsed["classification"] == "schedule":
+            calendar.schedule_event(self.calendar_service, self.parsed)
+
+            # Start our TwiML response
+            resp = MessagingResponse()
+
+            dt = datetime.fromisoformat(self.parsed["start"]["dateTime"])
+            start_time = dt.strftime("%Y-%m-%d at %-I:%M%p")
+
+            # Determine the right reply for this message
+            resp.message(
+                f"""Your event '{self.parsed["summary"]}' on {start_time} is scheduled"""
+            )
+
+            return HttpResponse(str(resp))
+
+        elif self.parsed["classification"] == "reminder":
+            print(self.parsed.keys())
+            event = self.ScheduledEvent(
+                summary=self.parsed["summary"],
+                location=self.parsed["location"],
+                description=self.parsed["description"],
+                start_time=self.parsed["start"]["dateTime"],
+                end_time=self.parsed["end"]["dateTime"],
+                phone_number=self.sender_phone_number,
+            )
+            event.save()
+            print("saved")
+
+            # Start our TwiML response
+            resp = MessagingResponse()
+
+            dt = datetime.fromisoformat(self.parsed["start"]["dateTime"])
+            start_time = dt.strftime("%Y-%m-%d at %-I:%M%p")
+
+            # Determine the right reply for this message
+            resp.message(
+                f"""Your reminder '{self.parsed["summary"]}' on {start_time} is scheduled"""
+            )
+
+            return HttpResponse(str(resp))
--- scripts/test_pipeline.py
+++ scripts/test_pipeline.py
@@ -1,65 +1,20 @@
 # %%
+from IPython import get_ipython
+
 if get_ipython() is not None:
     get_ipython().run_line_magic("load_ext", "autoreload")
     get_ipython().run_line_magic("autoreload", "2")
 from remindme import config, parsers
+from remindme.main import MessageHandler
 
 config.setup()
 
 # %%
 message_body = """
-    openai, what is 2+2?
+    schedule an appointment at 4pm to make cups.
 """.strip()
 
 # %%
-if message_body.startswith("openai"):
-    result = parsers.openai_standard(message_body)
-    str(result)
-
-# %%
-if not message_body.startswith("openai"):
-    parsed_event = parsers.get_gpt4_schedule_response(message_body)
-
-
-# %%
-if parsed_event["classification"] == "schedule":
-    calendar.schedule_event(calendar_service, parsed_event)
-
-    # Start our TwiML response
-    resp = MessagingResponse()
-
-    dt = datetime.fromisoformat(parsed_event["start"]["dateTime"])
-    start_time = dt.strftime("%Y-%m-%d at %-I:%M%p")
-
-    # Determine the right reply for this message
-    resp.message(
-        f"""Your event '{parsed_event["summary"]}' on {start_time} is scheduled"""
-    )
-
-    return HttpResponse(str(resp))
-
-elif parsed_event["classification"] == "reminder":
-    print(parsed_event.keys())
-    event = ScheduledEvent(
-        summary=parsed_event["summary"],
-        location=parsed_event["location"],
-        description=parsed_event["description"],
-        start_time=parsed_event["start"]["dateTime"],
-        end_time=parsed_event["end"]["dateTime"],
-        phone_number=sender_phone_number,
-    )
-    event.save()
-    print("saved")
-
-    # Start our TwiML response
-    resp = MessagingResponse()
-
-    dt = datetime.fromisoformat(parsed_event["start"]["dateTime"])
-    start_time = dt.strftime("%Y-%m-%d at %-I:%M%p")
-
-    # Determine the right reply for this message
-    resp.message(
-        f"""Your reminder '{parsed_event["summary"]}' on {start_time} is scheduled"""
-    )
-
-    return HttpResponse(str(resp))
+handler = MessageHandler(message_body=message_body)
+res = handler.parse_message()
+res
